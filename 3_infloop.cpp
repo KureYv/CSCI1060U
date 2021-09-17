8750c7162bbc484238270050ff7191113f8062b2
Matthew <matthewlee031205@gmail.com>
2021-09-17 16:59:25 -0400

    3rd lecture notes

---
+++ 3_infloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    while (number != -1)
+    {
+        cout << "Enter a  number: ";
+        cin >> number;
+        
+    } 
+    return 0;
+}
---
+++ 3_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a = 'a';
+    while(a <= 'z'){
+        cout << a++<<endl;
+        
+    } 
+    return 0;
+}
